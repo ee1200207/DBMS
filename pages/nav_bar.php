5924211a7309dd8958869ce0f542c6e21a80a83f
dthadi <dthadi939@gmail.com>
2014-11-06 20:30:00 +0530

    navigation bar

---
+++ pages/nav_bar.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+
+        li {
+            display: inline;
+
+        }
+        li.logout{
+            padding: 30cm;
+        }
+    </style>
+</head>
+<body>
+
+<ul>
+    <li><a href="#Courses">Courses</a>&nbsp;&nbsp;&nbsp;  </li>
+    <li><a href="#Upload">Project Upload</a>&nbsp;&nbsp;&nbsp;</li>
+    <li class="logout"><a href="logout.php">Logout</a> </li>
+
+</ul>
+
+</body>
+</html>
